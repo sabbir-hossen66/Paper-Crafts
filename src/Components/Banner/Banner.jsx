24c875eba040da70e1c446324be78e49eae5a6fb
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-05-02 22:22:44 +0600

    picture updated

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -10,9 +10,9 @@ const Banner = () => {
       <div className='max-w-screen-xl  mx-auto py-8'>
 
         <AwesomeSlider animation="cubeAnimation">
-          <div data-src="https://i.ibb.co/y5KkP00/banner1.jpg" />
-          <div data-src="https://i.ibb.co/6mzvc5N/banner2.jpg" />
           <div data-src="https://i.ibb.co/BgGGhWb/banner3.jpg" />
+          <div data-src="https://i.ibb.co/ZXzrDSP/nice-craft.jpg" />
+          <div data-src="https://i.ibb.co/6mzvc5N/banner2.jpg" />
         </AwesomeSlider>
 
       </div>
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -3,7 +3,7 @@ const Services = () => {
     <section className=" text-gray-800 py-8">
       <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
         <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-50">
-          <img src="https://source.unsplash.com/random/480x360" alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
+          <img src="https://i.ibb.co/BGMqBRw/craft1.jpg" alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
           <div className="p-6 space-y-2 lg:col-span-5">
             <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Our Services</h3>
             <span className="text-xs dark:text-gray-600">February 19, 2021</span>
@@ -12,7 +12,7 @@ const Services = () => {
         </a>
         <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
-            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?1" />
+            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://i.ibb.co/m6dTk4S/craft2jpeg.jpg" />
             <div className="p-6 space-y-2">
               <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">Our Team</h3>
               <span className="text-xs dark:text-gray-600">January 21, 2021</span>
@@ -20,7 +20,7 @@ const Services = () => {
             </div>
           </a>
           <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
-            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?2" />
+            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://i.ibb.co/R2zFfcg/craft3.jpg" />
             <div className="p-6 space-y-2">
               <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">Our Goal</h3>
               <span className="text-xs dark:text-gray-600">January 22, 2021</span>
@@ -28,7 +28,7 @@ const Services = () => {
             </div>
           </a>
           <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
-            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?3" />
+            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://i.ibb.co/x3nfbtq/craft4.jpg" />
             <div className="p-6 space-y-2">
               <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">Our MIssion</h3>
               <span className="text-xs dark:text-gray-600">January 23, 2021</span>
@@ -36,7 +36,7 @@ const Services = () => {
             </div>
           </a>
           <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
-            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?4" />
+            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://i.ibb.co/ZXzrDSP/nice-craft.jpg" />
             <div className="p-6 space-y-2">
               <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">Our Plan</h3>
               <span className="text-xs dark:text-gray-600">January 24, 2021</span>
@@ -46,7 +46,7 @@ const Services = () => {
           <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
             <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?5" />
             <div className="p-6 space-y-2">
-              <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
+              <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">Our Motto</h3>
               <span className="text-xs dark:text-gray-600">January 25, 2021</span>
               <p>Going to church is probably the most important thing people do in their lives. Many people center and build their lives around going and talking about what religion they associate with for a couple of hours.</p>
             </div>
